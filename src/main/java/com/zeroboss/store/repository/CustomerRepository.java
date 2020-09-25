7f91124bca351e04224281f96f25d461b699e3bc
brucieW <bruciew50@gmail.com>
2020-09-26 09:47:10 +1000

    Added jdl stuff.

---
+++ src/main/java/com/zeroboss/store/repository/CustomerRepository.java
@@ -0,0 +1,14 @@
+package com.zeroboss.store.repository;
+
+import com.zeroboss.store.domain.Customer;
+
+import org.springframework.data.jpa.repository.*;
+import org.springframework.stereotype.Repository;
+
+/**
+ * Spring Data  repository for the Customer entity.
+ */
+@SuppressWarnings("unused")
+@Repository
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+}
